c8f25730c106a1e4f22ae623ae4e26aa0f2bdfa0
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-05-02 11:08:46 +0300

    added a divider for the drawer

--- src/app/components/core/room/room.component.ts
+++ src/app/components/core/room/room.component.ts
@@ -79,9 +79,9 @@ export class RoomComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.messages = [];
-    console.log("pre-connect");
+    console.log('pre-connect');
     this.imageService.connect();
-    console.log("post-connect");
+    console.log('post-connect');
     console.log('triggered');
     this.triggerTimerSubscription = interval(10000).subscribe(x => {
       console.log('triggered');
