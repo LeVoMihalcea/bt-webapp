0a27d9beb695b06fb61089963358a6dfe7958166
LeonardM <leomihalcea@gmail.com>
2021-05-15 18:56:53 +0300

    changed the env socketUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   name: 'PROD',
   apiUrl: 'https://bt-backend.sudo.rocks',
-  socketUrl: 'https://bt-backend.sudo.rocks/analyse',
+  socketUrl: 'https://bt-backend.sudo.rocks/ws',
   agoraTokenApiUrl: 'https://bt-tokenizer.sudo.rocks',
   appId: '5b6c486a6cdc41e2829544fb59a24761'
 };
