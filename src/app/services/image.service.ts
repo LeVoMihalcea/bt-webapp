81d703f1228a071ba2522cdd6e5088cc384e4cec
LeonardM <leomihalcea@gmail.com>
2021-05-15 18:26:16 +0300

    finished chat implementation, only design to go

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -2,15 +2,15 @@ import {Injectable} from '@angular/core';
 import {environment} from '@environments/environment';
 import * as SockJS from 'sockjs-client';
 import {IFrame, Stomp} from '@stomp/stompjs';
-import {Message} from '@app/components/domain/Message';
+import {Reply} from '@app/components/domain/Reply';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
-  topic = '/topic/AI-responses';
+  topic = '/topic/messages';
   stompClient: any;
-  public messages: string[] = [];
+  public messages: Reply[] = [];
 
   constructor() {
   }
@@ -19,11 +19,8 @@ export class ImageService {
     const ws = new SockJS(environment.socketUrl);
     this.stompClient = Stomp.over(ws);
     const thisSnapshot = this;
-    console.log('pre-connect');
     thisSnapshot.stompClient.connect({}, (frame) => {
-      console.log('in-connect');
       thisSnapshot.stompClient.subscribe(thisSnapshot.topic, (sdkEvent) => {
-        console.log('on-message-received');
         thisSnapshot.onMessageReceived(sdkEvent);
       });
     }, this.errorCallBack);
@@ -49,7 +46,7 @@ export class ImageService {
   onMessageReceived(message: IFrame): void {
     console.log('Message Recieved from Server :: ' + message.body);
     console.log(this.messages);
-    // const parsedMessage = JSON.parse(message.body);
-    this.messages.push(message.body);
+    const parsedMessage = JSON.parse(message.body);
+    this.messages.push(parsedMessage);
   }
 }
